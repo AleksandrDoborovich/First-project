3a26e34b61b74688a2c631dd12d5257fd245b5ae
Aliaksei Belski <mr.belski@gmail.com>
2016-04-17 22:53:02 +0300

    Code format. minor changes (closure)

--- js1.js
+++ js1.js
@@ -1,82 +1,80 @@
-var myGoal;
-var enterGoal;
-var mainEl;
-var newEl;
-var checkBut;
-var deleteBut;
-var divTag;
+(function($) {
 
-var pEl;
+    var myGoal;
+    var enterGoal;
+    var mainEl;
+    var newEl;
+    var checkBut;
+    var deleteBut;
+    var divTag;
 
+    var pEl;
 
-//
-myGoal=document.getElementById("inputForm");
-myGoal.addEventListener("keydown", WriteGoal, false);
-function WriteGoal (){};
+    //
+    myGoal = document.getElementById("inputForm");
+    myGoal.addEventListener("keydown", WriteGoal, false);
+    function WriteGoal() {}
 
+    enterGoal = document.getElementById("enterNewGoal");
+    enterGoal.addEventListener("click", AddToPage, false);
 
-enterGoal=document.getElementById("enterNewGoal");
-enterGoal.addEventListener("click", AddToPage, false);
+    mainEl = document.getElementById("mainElement");
 
+    // Кнопка удаления выбранных целей.
+    // Обработчик нажатия клика вынесен из шаблона дял возможности работы замыкания.
+    var deleteSelected = $('#deleteSelectedGoals');
+    deleteSelected.on('click', DeleteGoals);
 
-mainEl=document.getElementById("mainElement");
+    function AddToPage() {
+        divTag = document.createElement("div");
+        mainEl.appendChild(divTag);
+        deleteBut = document.createElement("img");
+        deleteBut.src = "image/delete1.png";
+        deleteBut.onclick = deleteOne;
+        divTag.appendChild(deleteBut);
 
-function AddToPage (){
+        checkBut = document.createElement("input");
+        checkBut.type = "checkbox";
+        checkBut.class = "checkFamily";
+        checkBut.onclick = Overline;
+        divTag.appendChild(checkBut);
 
-divTag=document.createElement("div");
-mainEl.appendChild(divTag);
-deleteBut=document.createElement("img");
-deleteBut.src="image/delete1.png";
-deleteBut.onclick=deleteOne;
-divTag.appendChild(deleteBut);
+        pEl = document.createElement("span");
+        divTag.appendChild(pEl);
 
-checkBut=document.createElement("input");
-checkBut.type="checkbox";
-checkBut.class="checkFamily";
-checkBut.onclick=Overline;
-divTag.appendChild(checkBut);
+        newEl = document.createTextNode(myGoal.value);
+        pEl.appendChild(newEl);
 
-pEl=document.createElement("span");
-divTag.appendChild(pEl);
+        myGoal.value = "";
+    }
 
-newEl=document.createTextNode(myGoal.value);
-pEl.appendChild(newEl);
+    function Overline() {
+        var ch = document.getElementsByTagName("input");
+        var pEl2 = document.getElementsByTagName("span");
+        for (var i = 1; i < ch.length; i++) {
+            if (ch[i].checked) {
+                pEl2[i - 1].style.textDecoration = "line-through"
+            } else {
+                pEl2[i - 1].style.textDecoration = "none"
+            }
+        }
+    }
 
+    function DeleteGoals() {
+        var a = document.getElementsByTagName("input");
+        var b = document.getElementsByTagName("div");
 
-myGoal.value="";
-};
+        for (var i = 1; i < a.length; i++) {
+            if (a[i].checked) {
+                mainEl.removeChild(b[i])
+            }
+        }
 
+    }
 
-
-
-function Overline (){
-	var ch=document.getElementsByTagName("input");
-	var pEl2=document.getElementsByTagName("span");
-			for (i=1; i<ch.length; i++) {
-			if (ch[i].checked){pEl2[i-1].style.textDecoration="line-through"}
-				else{pEl2[i-1].style.textDecoration="none"}
-		}
-}
-
-
-function DeleteGoals() {
- 
-	var a=document.getElementsByTagName("input");
-	var b=document.getElementsByTagName("div");
-
-	
-			for (i=1; i<a.length; i++) {
-				if (a[i].checked){mainEl.removeChild(b[i])}
-			}	
-
-}
-
-
-
-
-
-function deleteOne() {
-var b=document.getElementsByTagName("div");
-var num=$('img').index(this);
-mainEl.removeChild(b[num+1]);
-}
+    function deleteOne() {
+        var b = document.getElementsByTagName("div");
+        var num = $('img').index(this);
+        mainEl.removeChild(b[num + 1]);
+    }
+})(jQuery);
