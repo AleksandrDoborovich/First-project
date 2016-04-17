9eeb7dd5a2bd0b4a816146f7f6c832744805021e
AleksandrDoborovich <aleksandr.doborovich@gmail.com>
2016-04-17 20:34:34 +0300

    third commit
    working variant

--- js1.js
+++ js1.js
@@ -2,6 +2,12 @@ var myGoal;
 var enterGoal;
 var mainEl;
 var newEl;
+var checkBut;
+var deleteBut;
+var divTag;
+
+var pEl;
+
 
 //
 myGoal=document.getElementById("inputForm");
@@ -14,14 +20,50 @@ enterGoal.addEventListener("click", AddToPage, false);
 
 
 mainEl=document.getElementById("mainElement");
+
 function AddToPage (){
 
+divTag=document.createElement("div");
+mainEl.appendChild(divTag);
+deleteBut=document.createElement("img");
+deleteBut.src="image/delete1.png";
+divTag.appendChild(deleteBut);
+
+checkBut=document.createElement("input");
+checkBut.type="checkbox";
+checkBut.class="checkFamily";
+checkBut.onclick=Overline;
+divTag.appendChild(checkBut);
+
+pEl=document.createElement("span");
+divTag.appendChild(pEl);
+
 newEl=document.createTextNode(myGoal.value);
-mainEl.appendChild(newEl)
+pEl.appendChild(newEl);
 
 };
 
 
 
 
+function Overline (){
+	var ch=document.getElementsByTagName("input");
+	var pEl2=document.getElementsByTagName("span");
+			for (i=1; i<ch.length; i++) {
+			if (ch[i].checked){pEl2[i-1].style.textDecoration="line-through"}
+				else{pEl2[i-1].style.textDecoration="none"}
+		}
+}
 
+
+function DeleteGoals() {
+ 
+	var a=document.getElementsByTagName("input");
+	var b=document.getElementsByTagName("div");
+
+	
+			for (i=1; i<a.length; i++) {
+				if (a[i].checked){mainEl.removeChild(b[i])}
+			}	
+
+}
